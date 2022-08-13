4f93e5c958feb358a344fff976ed98d7c6c1ca7e
Alana <halo2382001@yahoo.com>
2022-08-13 15:45:08 -0400

    added bio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,6 +5,7 @@ function About() {
   return (
     <section className="my-5">
       <h1 id="about">Hello World!</h1>
+      <p>Hi there! My name is Alana McKeel and I am currently progressing through the Full Stack Coding Boot Camp at Michigan State University. Professionally, I have had prior experience in Business and Hotel management as well as Title and Escrow experience. Currently, I am focused on securing a full-time position as a Full Stack Developer upon graduation (expected September 2022). As I have progressed thru the course, I have been drawn to the back end and have really enjoyed learning and understanding the processes. Thank you for visiting, let's connect!</p>
       <img
         src={coverImage}
         className="my-4"
