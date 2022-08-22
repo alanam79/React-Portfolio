4de3c7f2f5708173bcb4895850eba8b2b68f29b3
Alana <halo2382001@yahoo.com>
2022-08-22 12:00:11 -0400

    working on deploying page

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,29 +4,29 @@ function Nav() {
   return (
     <header className="flex-row px-1">
       <h2>
-        <a id="link" href="/">
+        <a id="link" href="/React-Portfolio/">
           Alana McKeel
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-1">
-            <a id="about" href="/">
+            <a id="about" href="/React-Portfolio/">
               About me
             </a>
           </li>
           <li className="mx-1">
-            <a id="about" href="/contact">
+            <a id="about" href="/React-Portfolio/contact">
               Contact
             </a>
           </li>
           <li className="mx-1">
-            <a id="about" href="/portfolio">
+            <a id="about" href="/React-Portfolio/portfolio">
               Portfolio
             </a>
           </li>
           <li className="mx-1">
-            <a id="about" href="/resume">
+            <a id="about" href="/React-Portfolio/resume">
               Resume
             </a>
           </li>
